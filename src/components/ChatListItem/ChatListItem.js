3a361c0773fed8c010664559ba380b69ebe85b0d
AndréPousa <andre.roberto.pousa@gmail.com>
2021-11-14 21:32:55 -0300

    chatList

--- src/components/ChatListItem/ChatListItem.js
+++ src/components/ChatListItem/ChatListItem.js
@@ -3,8 +3,19 @@ import './ChatListItem.css';
 
 const ChatListItem = () => {
   return (
-    <div>
-      ...
+    <div className="chatListItem">
+      <img className="chatListItem-avatar" src="https://previews.123rf.com/images/kritchanut/kritchanut1401/kritchanut140100054/25251050-businessman-avatar-profile-picture.jpg" alt=""/>
+      <div className="chatListItem-lines">
+        <div className="chatListItem-line">
+          <div className="chatListItem-name">André Roberto</div>
+          <div className="chatListItem-date">20:40</div>
+        </div>
+        <div className="chatListItem-line">
+          <div className="chatListItem-lastMsg">
+            <p>Olá tudo bém ? como estão as coisas ? E a musculação, voltou a treinar ? você está se tornando um ótimo programador.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
